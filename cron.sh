d8d87728c3826eb1e3632850d2cd08a5ea3f8438
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-08-30 15:57:44 -0300

    another big bag of changes

--- cron.sh
+++ cron.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+DAY=$(date +"%d")
+
+function is_prime() {
+    if [[ $1 -eq 2 ]] || [[ $1 -eq 3 ]]; then
+        return 1  # prime
+    fi
+
+    if [[ $(($1 % 2)) -eq 0 ]] || [[ $(($1 % 3)) -eq 0 ]]; then
+        return 0  # not a prime
+    fi
+
+    i=5; w=2
+
+    while [[ $((i * i)) -le $1 ]]; do
+        if [[ $(($1 % i)) -eq 0 ]]; then
+            return 0  # not a prime
+        fi
+        i=$((i + w))
+        w=$((6 - w))
+    done
+
+    return 1  # prime
+}
+
+# Exit if current day is a prime number
+is_prime $DAY
+if [[ $? -eq 1 ]]; then
+  exit 0
+fi
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BIN=./bin/readme
 
